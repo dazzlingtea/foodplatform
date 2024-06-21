1766bdbbdb7bcfc4caa1c9d50d06dab219f8984d
songjihoon12 <jjang1702@naver.com>
2024-06-21 17:48:22 +0900

    메인페이지 랜더링시 카테고리별로 상품조회 쿼리 (미완성임 생각중)

--- src/test/java/org/nmfw/foodietree/domain/product/repository/ProductMapperTest.java
+++ src/test/java/org/nmfw/foodietree/domain/product/repository/ProductMapperTest.java
@@ -3,14 +3,13 @@ package org.nmfw.foodietree.domain.product.repository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.nmfw.foodietree.domain.product.dto.response.ProductFindAllDto;
+import org.nmfw.foodietree.domain.product.mapper.ProductMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductMapperTest {
