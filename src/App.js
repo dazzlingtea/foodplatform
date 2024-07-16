a3caa4e663df992c6225a2094180d4fe39072c21
hoho <62678380+hoseong511@users.noreply.github.com>
2024-07-16 14:39:41 +0900

    fix reset-css (#3)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { RouterProvider } from "react-router-dom";
 import { router } from "./config/router-config";
+import {Reset} from "styled-reset";
 
 
 const App = () => {
   
   return (
-    <RouterProvider router={router} />
+      <>
+        <Reset />
+        <RouterProvider router={router} />
+      </>
   );
 };
 
