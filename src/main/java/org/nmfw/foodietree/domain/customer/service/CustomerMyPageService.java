d38866910b2ff07f5dd64c89b036ba006247a3b7
hoho <62678380+hoseong511@users.noreply.github.com>
2024-07-25 17:02:14 +0900

    SCRUM-46-customer-edit-PATCH-DELETE (#12)
    * SCRUM-46 customer-edit-controller
    
    - POST : preferred food, area, store
    - PATCH : nickname, customer_phone_number
    - DELETE : preferred food, area, store
    - /customer/edit
    
    * SCRUM-46 refactoring customer-edit-img
    
    * SCRUM-46 fix conflict customer-edit-img
    
    * SCRUM-46 refactoring customer-edit-img
    
    - 사용하지 않는 메소드 정리

--- src/main/java/org/nmfw/foodietree/domain/customer/service/CustomerMyPageService.java
+++ src/main/java/org/nmfw/foodietree/domain/customer/service/CustomerMyPageService.java
@@ -1,5 +1,6 @@
 package org.nmfw.foodietree.domain.customer.service;
 
+import java.io.File;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nmfw.foodietree.domain.customer.dto.resp.*;
@@ -8,13 +9,18 @@ import org.nmfw.foodietree.domain.customer.entity.ReservationDetail;
 import org.nmfw.foodietree.domain.customer.entity.value.IssueStatus;
 import org.nmfw.foodietree.domain.customer.entity.value.PickUpStatus;
 import org.nmfw.foodietree.domain.customer.mapper.CustomerMyPageMapper;
+import org.nmfw.foodietree.domain.product.Util.FileUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.nmfw.foodietree.domain.customer.entity.value.IssueCategory.fromString;
 import static org.nmfw.foodietree.domain.customer.entity.value.IssueStatus.*;
@@ -24,210 +30,196 @@ import static org.nmfw.foodietree.domain.customer.entity.value.IssueStatus.*;
 @Slf4j
 public class CustomerMyPageService {
 
-    private final CustomerMyPageMapper customerMyPageMapper;
-    private final PasswordEncoder encoder;
+	private final CustomerMyPageMapper customerMyPageMapper;
+	private final PasswordEncoder encoder;
+	@Value("${env.upload.path}")
+	private String uploadDir;
 
-    /**
-     * 고객 정보를 가져오는 메서드
-     * @param customerId 고객 ID
-     * @return 고객 정보 DTO
-     */
-    public CustomerMyPageDto getCustomerInfo(String customerId) {
-        CustomerMyPageDto customer = customerMyPageMapper.findOne(customerId);
-        List<String> preferenceAreas = customerMyPageMapper.findPreferenceAreas(customerId);
-        List<PreferredFoodDto> preferenceFoods = customerMyPageMapper.findPreferenceFoods(customerId);
-        List<CustomerFavStoreDto> favStore = customerMyPageMapper.findFavStore(customerId);
+	// customer 마이페이지 소비자 정보 조회 중간 처리
+	public CustomerMyPageDto getCustomerInfo(String customerId) {
 
-        return CustomerMyPageDto.builder()
-                .customerId(customer.getCustomerId())
-                .nickname(customer.getNickname())
-                .profileImage(customer.getProfileImage())
-                .customerPhoneNumber(customer.getCustomerPhoneNumber())
-                .preferredFood(preferenceFoods)
-                .preferredArea(preferenceAreas)
-                .favStore(favStore)
-                .build();
-    }
+		CustomerMyPageDto customer = customerMyPageMapper.findOne(customerId);
+		List<String> preferenceAreas = customerMyPageMapper.findPreferenceAreas(customerId);
+		List<PreferredFoodDto> preferenceFoods = customerMyPageMapper.findPreferenceFoods(customerId);
+		List<CustomerFavStoreDto> favStore = customerMyPageMapper.findFavStore(customerId);
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월dd일 HH시mm분");
-    /**
-     *
-     * @param customerId: 회원아이디를 전달받아
-     * @return MyPageReservationDetailDto List
-     */
-    public List<MyPageReservationDetailDto> getReservationInfo(String customerId) {
+		return CustomerMyPageDto.builder()
+			.customerId(customer.getCustomerId())
+			.nickname(customer.getNickname())
+			.profileImage(customer.getProfileImage())
+			.customerPhoneNumber(customer.getCustomerPhoneNumber())
+			.preferredFood(preferenceFoods)
+			.preferredArea(preferenceAreas)
+			.favStore(favStore)
+			.build();
+	}
 
-        List<ReservationDetail> reservations = customerMyPageMapper.findReservations(customerId);
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM월dd일 HH시mm분");
 
+	/**
+	 * @param customerId: 회원아이디를 전달받아
+	 * @return MyPageReservationDetailDto List
+	 */
+	public List<MyPageReservationDetailDto> getReservationInfo(String customerId) {
+
+		List<ReservationDetail> reservations = customerMyPageMapper.findReservations(customerId);
 
 //        List<MyPageReservationDetailDto> collect = reservations.stream().map(reservation -> {
 //                    MyPageReservationDetailDto dto = new MyPageReservationDetailDto(reservation);
 //                    dto.setStatus(determinePickUpStatus(reservation));
 //                    return dto;
 //                });
-        List<MyPageReservationDetailDto> collect1 = reservations.stream()
-                .map(reservation -> {
-                    MyPageReservationDetailDto dto = new MyPageReservationDetailDto(reservation);
-                    dto.setStatus(determinePickUpStatus(reservation));
-                    return dto;
-                }).collect(Collectors.toList());
+		List<MyPageReservationDetailDto> collect1 = reservations.stream()
+			.map(reservation -> {
+				MyPageReservationDetailDto dto = new MyPageReservationDetailDto(reservation);
+				dto.setStatus(determinePickUpStatus(reservation));
+				return dto;
+			}).collect(Collectors.toList());
 
-        List<MyPageReservationDetailDto> collect = reservations.stream()
-                .map(reservation -> MyPageReservationDetailDto.builder().status(determinePickUpStatus(reservation))
-                        .reservationId(reservation.getReservationId())
-                        .customerId(reservation.getCustomerId())
-                        .nickname(reservation.getNickname())
-                        .reservationTime(reservation.getReservationTime())
-                        .cancelReservationAt(reservation.getCancelReservationAt())
-                        .pickedUpAt(reservation.getPickedUpAt())
-                        .pickupTime(reservation.getPickupTime())
-                        .storeName(reservation.getStoreName())
-                        .storeImg(reservation.getStoreImg())
-                        .price(reservation.getPrice())
-                        .build()).collect(Collectors.toList());
+		List<MyPageReservationDetailDto> collect = reservations.stream()
+			.map(reservation -> MyPageReservationDetailDto.builder()
+				.status(determinePickUpStatus(reservation))
+				.reservationId(reservation.getReservationId())
+				.customerId(reservation.getCustomerId())
+				.nickname(reservation.getNickname())
+				.reservationTime(reservation.getReservationTime())
+				.cancelReservationAt(reservation.getCancelReservationAt())
+				.pickedUpAt(reservation.getPickedUpAt())
+				.pickupTime(reservation.getPickupTime())
+				.storeName(reservation.getStoreName())
+				.storeImg(reservation.getStoreImg())
+				.price(reservation.getPrice())
+				.build()).collect(Collectors.toList());
 
+		for (MyPageReservationDetailDto reservation : collect) {
+			log.info(reservation.toString());
+			if (reservation.getReservationTime() != null) {
+				reservation.setReservationTimeF(reservation.getReservationTime().format(formatter));
+			}
+			if (reservation.getCancelReservationAt() != null) {
+				reservation.setCancelReservationAtF(
+					reservation.getCancelReservationAt().format(formatter));
+			}
+			if (reservation.getPickedUpAt() != null) {
+				reservation.setPickedUpAtF(reservation.getPickedUpAt().format(formatter));
+			}
+			if (reservation.getPickupTime() != null) {
+				reservation.setPickupTimeF(reservation.getPickupTime().format(formatter));
+			}
+		}
+		return collect;
+	}
 
-        for (MyPageReservationDetailDto reservation : collect) {
-            log.info(reservation.toString());
-            if (reservation.getReservationTime() != null) {
-                reservation.setReservationTimeF(reservation.getReservationTime().format(formatter));
-            }
-            if (reservation.getCancelReservationAt() != null) {
-                reservation.setCancelReservationAtF(reservation.getCancelReservationAt().format(formatter));
-            }
-            if (reservation.getPickedUpAt() != null) {
-                reservation.setPickedUpAtF(reservation.getPickedUpAt().format(formatter));
-            }
-            if (reservation.getPickupTime() != null) {
-                reservation.setPickupTimeF(reservation.getPickupTime().format(formatter));
-            }
-        }
-        return collect;
-    }
+	public PickUpStatus determinePickUpStatus(ReservationDetail reservation) {
+		if (reservation.getPickedUpAt() != null) {
+			return PickUpStatus.PICKEDUP;
+		} else if (reservation.getCancelReservationAt() != null) {
+			return PickUpStatus.CANCELED;
+		} else if (reservation.getPickupTime().isBefore(LocalDateTime.now())) {
+			return PickUpStatus.NOSHOW;
+		} else {
+			return PickUpStatus.RESERVED;
+		}
+	}
 
-    public PickUpStatus determinePickUpStatus(ReservationDetail reservation) {
-        if (reservation.getPickedUpAt() != null) {
-            return PickUpStatus.PICKEDUP;
-        } else if (reservation.getCancelReservationAt() != null) {
-            return PickUpStatus.CANCELED;
-        } else if (reservation.getPickupTime().isBefore(LocalDateTime.now())) {
-            return PickUpStatus.NOSHOW;
-        } else {
-            return PickUpStatus.RESERVED;
-        }
-    }
+	public boolean updateCustomerInfo(String customerId, List<UpdateDto> updates) {
+		for (UpdateDto update : updates) {
+			String type = update.getType();
+			String value = update.getValue();
+			log.info("update type: {}, value: {}", type, value);
+			if ("preferredFood".equals(type)) {
+				customerMyPageMapper.addPreferenceFood(customerId, value);
+				return true;
+			} else if ("preferredArea".equals(type)) {
+				customerMyPageMapper.addPreferenceArea(customerId, value);
+				return true;
+			} else if ("favStore".equals(type)) {
+				customerMyPageMapper.addFavStore(customerId, value);
+				return true;
+			} else {
+				customerMyPageMapper.updateCustomerInfo(customerId, type, value);
+				return true;
+			}
+		}
+		return false;
+	}
 
+	public boolean deleteCustomerInfo(String customerId, List<UpdateDto> dtos) {
+		for (UpdateDto delete : dtos) {
+			String type = delete.getType();
+			String target = delete.getValue();
 
-    public List<CustomerIssueDetailDto> getCustomerIssues(String customerId) {
-        List<CustomerIssues> issues = customerMyPageMapper.findIssues(customerId);
+			log.info("delete type: {}, target: {}", type, target);
 
-        return issues.stream().map(issue -> CustomerIssueDetailDto.builder()
-                .customerId(issue.getCustomerId())
-                .nickname(issue.getNickname())
-                .storeName(issue.getStoreName())
-                .issueCategory(fromString(issue.getIssueCategory()))
-                .issueStatus(checkIssueStatus(issue.getIssueCompleteAt()))
-                .issueCompleteAt(issue.getIssueCompleteAt())
-                .issueText(issue.getIssueText())
-                .cancelIssueAt(issue.getCancelIssueAt())
-                .build()
-        ).collect(Collectors.toList());
-    }
+			if ("preferredFood".equals(type)) {
+				customerMyPageMapper.deletePreferenceFood(customerId, target);
+				return true;
+			}
+			if ("preferredArea".equals(type)) {
+				customerMyPageMapper.deletePreferenceArea(customerId, target);
+				return true;
+			}
+			if ("favStore".equals(type)) {
+				customerMyPageMapper.deleteFavStore(customerId, target);
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * 이슈가 해결되어 시간이 있다면 -> Solved(해결완료)
-     * 접수된 시간이 있다면 -> INPROGRESS(진행중)
-     * @param issueCompleteAt : 이슈가 해결된 시간
-     * @return Status enum
-     */
-    private IssueStatus checkIssueStatus(LocalDateTime issueCompleteAt) {
-        if (issueCompleteAt == null) {
-            return INPROGRESS;
-        }else{
-            return SOLVED;
-        }
-    }
+	public boolean updateCustomerPw(String customerId, String newPassword) {
+		String encodedPw = encoder.encode(newPassword);
+		customerMyPageMapper.updateCustomerInfo(customerId, "customer_password", encodedPw);
+		return true;
+	}
 
-    public boolean updateCustomerInfo(String customerId, List<UpdateDto> updates) {
-        for (UpdateDto update : updates) {
-            String type = update.getType();
-            String value = update.getValue();
-            log.info("update type: {}, value: {}", type, value);
-            if ("preferredFood".equals(type)) {
-                customerMyPageMapper.addPreferenceFood(customerId, value);
-                return true;
-            }
-            else if("preferredArea".equals(type)) {
-                customerMyPageMapper.addPreferenceArea(customerId, value);
-                return true;
-            }
-            else if("favStore".equals(type)) {
-                customerMyPageMapper.addFavStore(customerId, value);
-                return true;
-            }
-            else{
-                customerMyPageMapper.updateCustomerInfo(customerId, type, value);
-                return true;
-            }
-        }
-        return false;
-    }
+	public StatsDto getStats(String customerId) {
+		List<ReservationDetail> reservations = customerMyPageMapper.findReservations(customerId);
 
-    public boolean deleteCustomerInfo(String customerId, List<UpdateDto> dtos) {
-        for (UpdateDto delete : dtos) {
-            String type = delete.getType();
-            String target = delete.getValue();
+		// 예약 내역 중 pickedUpAt이 null이 아닌 것들의 리스트
+		List<ReservationDetail> pickedUpReservations = reservations.stream()
+			.filter(reservation -> reservation.getPickedUpAt() != null)
+			.collect(Collectors.toList());
 
-            log.info("delete type: {}, target: {}", type, target);
+		// pickedUpAt이 null이 아닌 것들의 개수
+		int total = pickedUpReservations.size();
 
-            if("preferredFood".equals(type)) {
-                customerMyPageMapper.deletePreferenceFood(customerId, target);
-                return true;
-            }
-            if("preferredArea".equals(type)) {
-                customerMyPageMapper.deletePreferenceArea(customerId, target);
-                return true;
-            }
-            if("favStore".equals(type)) {
-                customerMyPageMapper.deleteFavStore(customerId, target);
-                return true;
-            }
-        }
-        return false;
-    }
+		// CO2 계산
+		double coTwo = total * 0.12;
 
-    public boolean updateCustomerPw(String customerId, String newPassword) {
-        String encodedPw = encoder.encode(newPassword);
-        customerMyPageMapper.updateCustomerInfo(customerId,"customer_password", encodedPw);
-        return true;
-    }
+		// totalPrice 계산
+		int totalPrice = pickedUpReservations.stream()
+			.mapToInt(ReservationDetail::getPrice)
+			.sum();
 
-    public StatsDto getStats(String customerId){
-        List<ReservationDetail> reservations = customerMyPageMapper.findReservations(customerId);
+		// money 계산
+		int money = (int) (totalPrice * 0.7);
 
-        // 예약 내역 중 pickedUpAt이 null이 아닌 것들의 리스트
-        List<ReservationDetail> pickedUpReservations = reservations.stream()
-                .filter(reservation -> reservation.getPickedUpAt() != null)
-                .collect(Collectors.toList());
+		return StatsDto.builder()
+			.total(total)
+			.coTwo(coTwo)
+			.money(money)
+			.build();
+	}
 
-        // pickedUpAt이 null이 아닌 것들의 개수
-        int total = pickedUpReservations.size();
-
-        // CO2 계산
-        double coTwo = total * 0.12;
-
-        // totalPrice 계산
-        int totalPrice = pickedUpReservations.stream()
-                .mapToInt(ReservationDetail::getPrice)
-                .sum();
-
-        // money 계산
-        int money = (int) (totalPrice * 0.7);
-
-        return StatsDto.builder()
-                .total(total)
-                .coTwo(coTwo)
-                .money(money)
-                .build();
-    }
+	public boolean updateProfileImg(String customerId, MultipartFile customerImg) {
+		try {
+			if (!customerImg.isEmpty()) {
+				File dir = new File(uploadDir);
+				if (!dir.exists()) {
+					dir.mkdirs();
+				}
+				String imagePath = FileUtil.uploadFile(uploadDir, customerImg);
+				UpdateDto dto = UpdateDto.builder()
+					.type("profile_image")
+					.value(imagePath)
+					.build();
+				updateCustomerInfo(customerId, List.of(dto));
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
