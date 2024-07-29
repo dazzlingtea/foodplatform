a8879437e8b069273d0d482d5556538fcc0bd8bb
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-07-29 18:09:59 +0900

    WIP jpa convert

---
+++ src/main/java/org/nmfw/foodietree/domain/issue/entiry/Issue.java
@@ -0,0 +1,40 @@
+package org.nmfw.foodietree.domain.issue.entiry;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_issue")
+@Setter @Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Issue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "issue_id")
+    private int issueId;
+
+    @Column(name = "issue_category")
+    private String issueCategory;
+
+    @Column(name = "issue_complete_at")
+    private LocalDateTime issueCompleteAt;
+
+    @Column(name = "issue_text")
+    private String issueText;
+
+    @Column(name = "cancel_issue_at")
+    private LocalDateTime cancelIssueAt;
+
+    @Column(name = "customer_id")
+    private String customerId;
+
+    @Column(name = "reservation_id")
+    private int reservationId;
+
+}
