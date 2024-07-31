a09c8133a30056e4a9c7f1e6cb9c639e57530149
Hansol Kim <160578362+hansolkim9@users.noreply.github.com>
2024-07-31 14:52:48 +0900

    Scrum 78 reservation jpa (#31)
    * # switch to Reservation entity JPA
    
    * # create ReservationRepository interface

--- src/main/java/org/nmfw/foodietree/domain/reservation/entity/Reservation.java
+++ src/main/java/org/nmfw/foodietree/domain/reservation/entity/Reservation.java
@@ -1,20 +1,39 @@
 package org.nmfw.foodietree.domain.reservation.entity;
 
 import lombok.*;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tbl_reservation")
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
     private int reservationId;
+
+    @Column(name = "customer_id", nullable = false)
     private String customerId;
-    private String productId;
+
+    @Column(name = "product_id", nullable = false)
+    private int productId;
+
+    @Column(name = "reservation_time", nullable = false)
     private LocalDateTime reservationTime;
+
+    @Column(name = "cancel_reservation_at")
     private LocalDateTime cancelReservationAt;
+
+    @Column(name = "picked_up_at")
     private LocalDateTime pickedUpAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
     private ReservationStatus status;
 }
---
+++ src/main/java/org/nmfw/foodietree/domain/reservation/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package org.nmfw.foodietree.domain.reservation.repository;
+
+import org.nmfw.foodietree.domain.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+}
