1d7908812359c49e3ecdf04dcb8ea65994e1129b
Maybaba <161430857+Maybaba@users.noreply.github.com>
2024-08-14 16:51:07 +0900

    Scrum 122 header getuser info design (#110)
    * # productDetail에 productImg 빠짐
    
    * # Selected Area 세션스토리지 저장
    
    * # 카테고리 별 세션 스토리지 주소값에 해당하는 가게 리스트 필터링
    
    * # selectedArea를 포함하는 가게 필터링
    
    * # modified CategotiesPage fetch to authFetch
    
    * token user Info render at header.js
    
    * # add navigate to mypage at icon
    
    * # fetch 시 localstorage에 userImage 저장
    
    * # username 불러오기
    
    ---------
    
    Co-authored-by: hyerin11 <hodolin24@naver.com>
    Co-authored-by: Riin <160578098+hyerin11@users.noreply.github.com>

---
+++ src/components/header/MyInfo.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faStore } from "@fortawesome/free-solid-svg-icons";
+import styles from "./MyInfo.module.scss";
+import { useNavigate } from "react-router-dom";
+import {getRefreshToken, getSubName, getToken, getUserRole} from "../../utils/authUtil";
+
+// 내 정보 들어가기
+const MyInfo = () => {
+    const [userInfo, setUserInfo] = useState(null); // 사용자 정보를 저장할 상태
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const response = await fetch(`/user/info`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ` + getToken(),
+                        "refreshToken" : getRefreshToken()
+                    }
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log("fetched userInfo data : ", data);
+
+                    // 유저 타입에 따라 다른 키와 값을 로컬 스토리지에 저장
+                    if (getUserRole() === 'store') {
+                        localStorage.setItem('userImage', data.productImg);
+                    } else if (getUserRole() === 'customer') {
+                        localStorage.setItem('userImage', data.profileImage);
+                    }
+
+                    // 닉네임이 null 일 경우 저장하지 않음
+                    if (getSubName() !== null) {
+                        localStorage.setItem('subName', data.subName);
+                    }
+
+                    setUserInfo(data);
+                } else {
+                    console.error("Failed to fetch user info");
+                }
+            } catch (error) {
+                console.error("Error fetching user info", error);
+            }
+        };
+
+        fetchUserInfo();
+    }, []); // 빈 배열로 설정해 컴포넌트가 마운트될 때 한 번만 호출되도록 함
+
+    const handleIconClick = (path) => {
+        navigate(path);
+    };
+
+    if (!userInfo) {
+        return null; // 로딩 중이거나 정보가 없을 때는 아무것도 렌더링하지 않음
+    }
+
+    return (
+        <div className={styles.myInfoContainer}>
+            <span className={styles.myInfo}> 안녕하세요 {getSubName() ? getSubName() : userInfo.email}님!</span>
+            <div className={styles.myIconContainer}>
+                {getUserRole() === 'store' ? (
+                    <>
+                        {/* Store 아이콘과 프로필 이미지 */}
+                        <img
+                            src={userInfo.productImg}
+                            alt="Store Profile"
+                            className={styles.profileImage}
+                            onClick={() => handleIconClick("/store")}
+                        />
+                    </>
+                ) : getUserRole() === 'customer' ? (
+                    <>
+                        {/* Customer 아이콘과 프로필 이미지 */}
+                        <img
+                            src={userInfo.profileImage}
+                            alt="Customer Profile"
+                            className={styles.profileImage}
+                            onClick={() => handleIconClick("/customer")}
+                        />
+                    </>
+                ) : null}
+            </div>
+        </div>
+    );
+};
+
+export default MyInfo;
--- src/utils/authUtil.js
+++ src/utils/authUtil.js
@@ -1,100 +1,44 @@
 
 
 import { jwtDecode } from 'jwt-decode';
-import { redirect } from "react-router-dom";
-
-// 로그인한 유저의 정보 가져오기
-export const getUserData = () => {
-    const userDataJson = localStorage.getItem('token');
-
-    return userDataJson;
-};
-
+// 로컬스토리지 토큰 가져오기
 export const getToken = () => {
     const token = localStorage.getItem('token');
 
     return token;
 }
 
+// 로컬스토리지 리프레시 토큰 가져오기
 export const getRefreshToken = () => {
     const refreshToken = localStorage.getItem('refreshToken');
 
     return refreshToken;
 }
 
-// 로컬스토리지에서 이메일 가져오기
+// 인증 토큰에서 이메일 가져오기
 export const getUserEmail = () => {
-    const userEmail = localStorage.getItem("email");
+    const token = localStorage.getItem('token');
+    const tokenInfo = jwtDecode(token);
+    const userEmail = tokenInfo.sub;
 
     return userEmail;
 };
 
 // 인증 토큰에서 role 가져오기
 export const getUserRole = () => {
-    const userType = localStorage.getItem("userType");
+    const token = localStorage.getItem('token');
+    const tokenInfo = jwtDecode(token);
+    const userType = tokenInfo.role;
 
     return userType;
 };
 
+// 로컬스토리지에서 닉네임(subName, nickName) 가져오기
+export const getSubName = () => {
+    const subName = localStorage.getItem("subName");
 
-// 로그인 회원정보를 불러오는 loader
-export const userDataLoader = () => {
-    console.log('userDataLoader call!');
-    return getUserData();
-};
-
-// 접근 권한을 확인하는 loader
-export const authCheckLoader = () => {
-    const userData = getUserData();
-
-    if(!userData) {
-        alert('로그인이 필요한 서비스 입니다');
-        return redirect('/sign-in');
-    }
-    return null; // 현재페이지에 머묾...
+    return subName;
 }
-
-
-// 이메일 토큰 판별
-export const verifyTokenLoader = async ({ request }) => {
-    const url = new URL(request.url);
-    const token = url.searchParams.get('token');
-    const refreshToken = url.searchParams.get('refreshToken');
-
-    console.log('token, refreshtoken', token, refreshToken);
-
-    if (token && refreshToken) {
-        const response = await fetch(`/email/verifyEmail`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ token, refreshToken }),
-        });
-
-        // 응답 상태 코드와 메시지 로그
-        console.log('Response Status:', response.status);
-        console.log('Response Status Text:', response.statusText);
-
-        if (!response.ok) {
-            // 응답 본문에서 에러 메시지 로그
-            const errorData = await response.json();
-            console.error('Error Response Data:', errorData);
-            throw new Error('Token verification failed');
-        }
-
-        const data = await response.json();
-        console.log('Response Data:', data);
-
-        if (data.success) {
-            localStorage.setItem('token', data.token);
-            localStorage.setItem('refreshToken', data.refreshToken);
-            return { email: data.email, userType: data.role, storeApprove: data.storeApprove };
-        }
-    }
-
-    throw new Error('Token is missing or invalid');
-};
 
 /**
  * 로그아웃 기능
@@ -127,6 +71,7 @@ export const checkAuthToken = async (navigate) => {
         navigate('/sign-in');
         return null;
     }
+    // 로컬스토리지 usertype 조작해도 접근 제한 가능
     const tokenInfo = jwtDecode(token);
     const userType = tokenInfo.role;
     const email = tokenInfo.sub;
@@ -155,7 +100,6 @@ export const checkLoggedIn = (navigate, currentPath) => {
         navigate('/sign-in');
         return null;
     }
-
     // access token이 있을 때
     try {
         const tokenInfo = jwtDecode(token);
@@ -210,3 +154,46 @@ export const authFetch = async (url, req) => {
     }
     return await fetch(url , init);
 }
+
+
+// 이메일 토큰 판별
+export const verifyTokenLoader = async ({ request }) => {
+    const url = new URL(request.url);
+    const token = url.searchParams.get('token');
+    const refreshToken = url.searchParams.get('refreshToken');
+
+    console.log('token, refreshtoken', token, refreshToken);
+
+    if (token && refreshToken) {
+        const response = await fetch(`/email/verifyEmail`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token, refreshToken }),
+        });
+
+        // 응답 상태 코드와 메시지 로그
+        console.log('Response Status:', response.status);
+        console.log('Response Status Text:', response.statusText);
+
+        if (!response.ok) {
+            // 응답 본문에서 에러 메시지 로그
+            const errorData = await response.json();
+            console.error('Error Response Data:', errorData);
+            throw new Error('Token verification failed');
+        }
+
+        const data = await response.json();
+        console.log('Response Data:', data);
+
+        if (data.success) {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('refreshToken', data.refreshToken);
+            return { email: data.email, userType: data.role, storeApprove: data.storeApprove };
+        }
+    }
+
+    throw new Error('Token is missing or invalid');
+};
+
