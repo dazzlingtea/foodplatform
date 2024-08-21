883eda5841d5cc9e16af740e1423c3d2e3ae905b
Jeong-in Seo <162551171+munginisfree@users.noreply.github.com>
2024-08-22 03:14:41 +0900

    add websocketTransport setting (#114)

--- src/main/java/org/nmfw/foodietree/global/config/WebSocketConfig.java
+++ src/main/java/org/nmfw/foodietree/global/config/WebSocketConfig.java
@@ -6,12 +6,13 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${env.domain:http://localhost:3000,http://localhost:3001,http://localhost:3002}")
+    @Value("${env.domain:http://localhost:3000,http://localhost:3001,http://localhost:3002,http://172.30.1.14:3000}")
     private String[] allowedOrigins;
 
     @Override
@@ -29,5 +30,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/noti")
                 .setAllowedOrigins(allowedOrigins)
                 .withSockJS();
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(2048 * 1024);
+        registration.setSendBufferSizeLimit(2048 * 1024);
+        registration.setSendTimeLimit(20 * 10000);
     }
 }
