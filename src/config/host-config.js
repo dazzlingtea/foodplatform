88fe0333e6f05abe8781cadaca7c875005ce9817
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-08-26 01:40:31 +0900

    host config foodietree url

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -14,10 +14,11 @@ const CHAT = '/chat';
 const NOTIFICATION = '/notification';
 
 const EC2_PUBLIC_IP = "http://3.38.5.29:";
+const FOODIETREE_SHOP_URL = "http://foodietree.shop"; // 배포환경
 
 export const BASE_URL = window.location.origin;
 const LOCAL_PORT = 8083; // 백엔드 로컬 서버 포트번호
-export const BACK_HOST = EC2_PUBLIC_IP+LOCAL_PORT;
+export const BACK_HOST = FOODIETREE_SHOP_URL;
 
 export const STORE_URL =  BACK_HOST+STORE;
 export const CUSTOMER_URL = BACK_HOST+CUSTOMER;
