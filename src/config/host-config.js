3430b609dde8241ce78649aad7c733b38a16f49a
EC2 Default User <ec2-user@ip-172-31-4-77.ap-northeast-2.compute.internal>
2024-08-25 04:19:50 +0000

    config and package proxy set

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -13,9 +13,11 @@ const USER = '/user';
 const CHAT = '/chat';
 const NOTIFICATION = '/notification';
 
+const EC2_PUBLIC_IP = "http://3.38.5.29:";
+
 export const BASE_URL = window.location.origin;
 const LOCAL_PORT = 8083; // 백엔드 로컬 서버 포트번호
-export const BACK_HOST = 'http://localhost:'+LOCAL_PORT;
+export const BACK_HOST = EC2_PUBLIC_IP+LOCAL_PORT;
 
 export const STORE_URL =  BACK_HOST+STORE;
 export const CUSTOMER_URL = BACK_HOST+CUSTOMER;
