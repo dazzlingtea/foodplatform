5423c303400dea1b714766d8e09b1dafbe0f2250
dazzlingtea <kitsnr08@gmail.com>
2024-08-26 14:27:45 +0900

    add crossorigin domain url

--- src/main/java/org/nmfw/foodietree/global/config/CrossOriginConfig.java
+++ src/main/java/org/nmfw/foodietree/global/config/CrossOriginConfig.java
@@ -14,6 +14,7 @@ public class CrossOriginConfig implements WebMvcConfigurer {
             "http://localhost:3002",
             "http://localhost:3003",
 	    "http://3.38.5.29:3000",
+            "https://foodietree.shop",
     };
 
     @Override
