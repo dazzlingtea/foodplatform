dc9f613229f39da2b5fe999e5dcd9c9dbed18354
Maybaba <sinyunjong@gmail.com>
2024-09-01 16:37:32 +0900

    # profile image fetch to review.js

--- src/main/java/org/nmfw/foodietree/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/nmfw/foodietree/domain/review/repository/ReviewRepository.java
@@ -19,18 +19,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRep
 
     List<MyReviewDto> findEnableReviewsByCustomerId(String customerId);
 
-    @Query("SELECT new org.nmfw.foodietree.domain.review.dto.res.ReviewDetailDto(" +
-            "r.reservationId, " +
-            "r.customerId, " +
-            "r.product.store.storeImg, " +
-            "r.reviewScore, " +
-            "r.reviewImg, " +
-            "r.reviewContent, " +
-            "r.product.store.storeId, " +  // storeId는 String 타입이어야 합니다.
-            "r.product.store.storeName, " +
-            "r.product.store.address) " +
-            "FROM Review r")
-    List<ReviewDetailDto> findAllReviewDetails();
-
 
 }
